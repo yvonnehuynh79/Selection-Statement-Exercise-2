cb6b25d3e61bd7a642459ca114e54a070364ae01
yvonnehuynh79 <yvonnehuynh888@gmail.com>
2023-04-12 12:00:34 -0700

    first commit

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(" what is your favorite school subject?");
+            string answer = Console.ReadLine();
+            switch (answer.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("me too");
+                    break;
+                case "nail class":
+                    Console.WriteLine("oh you can see me for that");
+                    break;
+                case "history":
+                    Console.WriteLine(" I'm not good with that");
+                    break;
+                case "science":
+                    Console.WriteLine("Interesting!");
+                    break;
+                case "pe":
+                    Console.WriteLine("i don't like it");
+                    break;
+                default:
+                    Console.WriteLine("ooh ok");
+                    break;
+
+            }
         }
     }
 }
